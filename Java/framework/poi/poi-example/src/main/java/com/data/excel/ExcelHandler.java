b1811b5f95b07c9c2d7d3cbe102f26cff8d32f01
Zhang Donghan <944369375@QQ.COM>
2018-04-02 20:43:10 +0800

    add xssf write and read example

--- Java/framework/poi/poi-example/src/main/java/com/data/excel/ExcelHandler.java
+++ Java/framework/poi/poi-example/src/main/java/com/data/excel/ExcelHandler.java
@@ -50,5 +50,50 @@ public class ExcelHandler {
 		workbook.close();
 		fis.close();
 	}
+	
+	/**
+	 * HSSF is the POI Project's pure Java implementation of the Excel '97(-2007)
+	 * file format. XSSF is the POI Project's pure Java implementation of the Excel
+	 * 2007 OOXML (.xlsx) file format.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void XSSFWriteExampl() throws IOException {
+		// create xssf type workbook
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		// create sheet
+		XSSFSheet sheet = workbook.createSheet("hello");
+		// create row
+		XSSFRow row = sheet.createRow(0);
+		// create cell
+		XSSFCell cell = row.createCell(2);
+		// set cell value
+		cell.setCellValue("Hello Excel");
+		// create output stream
+		FileOutputStream fos = new FileOutputStream(new File("E:\\XSSFWriteExample.xls"));
+		workbook.write(fos);
+		workbook.close();
+		fos.close();
+	}
+
+	@Test
+	public void XSSFReadExample() throws IOException {
+		// create input stream
+		FileInputStream fis = new FileInputStream(new File("E:\\\\XSSFWriteExample.xls"));
+		// create workbook
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		// get sheet
+		XSSFSheet sheet = workbook.getSheet("hello");
+		// get row
+		XSSFRow row = sheet.getRow(0);
+		// create cell
+		XSSFCell cell = row.getCell(2);
+		// set cell value
+		String cellValue = cell.getStringCellValue();
+		System.out.println("content: " + cellValue);
+		workbook.close();
+		fis.close();
+	}
 
 }
